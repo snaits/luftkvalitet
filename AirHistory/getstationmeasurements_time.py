59098a5089bc478fe2bfb0af73b1c5d64dc91ee2
groharam <56562314+groharam@users.noreply.github.com>
2019-10-19 13:16:50 +0200

    Add files via upload

---
+++ AirHistory/getstationmeasurements_time.py
@@ -0,0 +1,39 @@
+import json
+import requests
+import datetime
+import os
+import os.path
+from pathlib import Path
+from dateutil.relativedelta import relativedelta
+
+dirPath = Path('output')
+dateFormat = "%Y-%m-%d"
+
+currentDate = datetime.datetime.now()
+month = relativedelta(months=+1)
+startDate = datetime.datetime(2008,1,1)
+
+if not os.path.exists(dirPath):
+    os.mkdir(dirPath)
+
+while startDate < currentDate:
+    endDate = startDate + month
+
+    url = f'https://api.nilu.no/obs/historical/{startDate.strftime(dateFormat)}/{endDate.strftime(dateFormat)}/all'
+    print(url)
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    contents = response.text
+
+    fileName = f'timesmiddel-{startDate.strftime(dateFormat)}.json'
+    filePath = os.path.join(dirPath, fileName)
+
+    with open(filePath, mode="w", encoding="utf-8") as file: 
+        file.write(contents)
+
+    startDate = endDate
+
+
+
